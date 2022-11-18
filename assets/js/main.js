e4aee57248649fb7c93cba9563e0cda618421a0e
olgajuanne <olgajdango@gmail.com>
2022-11-18 19:38:46 -0300

    type and colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
 
-
 function convertPokemonToLi(pokemon) {
   return `
-      <li class="pokemon">
-      <span class="number">${pokemon.id}</span>
+      <li class="pokemon ${pokemon.type}">
+      <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
       <div class="detail">
         <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
+          ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+        <img src="${pokemon.photo}" 
             alt="${pokemon.name}">
       </div>
     </li>
@@ -20,7 +18,8 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('') 
+  const newHtml = pokemons.map(convertPokemonToLi).join('')
+  pokemonList.innerHTML = newHtml
 
 })
 
