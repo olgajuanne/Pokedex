e873f167764a13f19a1663b0fa16cd2dfe2675ef
olgajuanne <olgajdango@gmail.com>
2022-11-22 23:16:02 -0300

    form and input

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,24 @@ const maxRecords = 251
 const limit = 9
 let offset = 0;
 
+
 function about() {
   location.href = "about.html";
+    const nomePokemon = document.getElementsByName('#numb').value;
+    document.getElementsByName('valorDigitado').innerHTML = nomePokemon; 
+    console.log(nomePokemon)
 }
+
+const btn = document.querySelector('#send')
+  btn.addEventListener("click", function(e){
+
+    e.preventDefault();
+    const numb = document.querySelector("#numb");
+
+    const value = numb.value;
+    console.log(value);
+  });
+
 
 
 function loadPokemonItens(offset, limit) {
@@ -14,7 +29,7 @@ function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
       const newHtml = pokemons.map((pokemon) => `
         <li class="pokemon ${pokemon.type}">
-          <span class="number"><a id"about" onclick="about()" href="#">#${pokemon.number}</a></span>
+          <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
           <div class="detail">
             <ol class="types">
